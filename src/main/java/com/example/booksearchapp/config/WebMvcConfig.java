e6d934ebe2fb4744709ac49cd841ea679c4fd706
matsukawa-s <syoudai.hand@gmail.com>
2022-05-30 23:48:33 +0900

    :hammer: CORSを設定ファイルから読み込む

--- src/main/java/com/example/booksearchapp/config/WebMvcConfig.java
+++ src/main/java/com/example/booksearchapp/config/WebMvcConfig.java
@@ -1,18 +1,29 @@
 package com.example.booksearchapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig {
+    @Autowired
+    private Environment env;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(final CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
+                String corsAllowedUrl = env.getProperty("cors.allowed-url");
+
+                registry.addMapping("/**")
+                        .allowedOrigins(corsAllowedUrl)
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                ;
             }
         };
     }
